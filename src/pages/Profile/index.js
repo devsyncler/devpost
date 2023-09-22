45105f73126a82435e68dfa99f54751194cf102a
syncler <synclaprogrammer@gmail.com>
2023-09-22 09:56:17 -0300

    cadastrando user com firebase configurado

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Profile(){
+  return(
+    <View>
+      <Text>TELA perfil</Text>
+    </View>
+  )
+}
+
+export default Profile;
